900ea1e09cfef3890b48cca418b85b74b89a5e41
Alin <alin.cristian977@gmail.com>
2026-03-15 20:04:49 +0100

    added crème

--- FrenchNouns/Constants.cs
+++ FrenchNouns/Constants.cs
@@ -317,6 +317,7 @@ public class Constants
     #endregion
 
     #region "C" Nouns
+    public const string Creme = MasculineArticle + Space + "crème";
     public const string Cornichon = MasculineArticle + Space + "cornichon";
     public const string Crue = FeminineArticle + Space + "crue";
     public const string Coquillage = MasculineArticle + Space + "coquillage";
